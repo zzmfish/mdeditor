4f891dd582e4085c98db3f610ebc777540a4ae7b
周志明 <zhouzhiming@zhouzhimingdeMacBook-Air.local>
2014-12-06 16:32:42 +0800

    打开文件输入框选择或输入的文件

--- res/js/toolbar.js
+++ res/js/toolbar.js
@@ -5,8 +5,16 @@ var Toolbar = {
             source: files,
             minLength: 0,
             delay: 100,
+            select: function(e, ui) {
+                EditFile(ui.item.value);
+            }
         }).click(function() {
             $(this).autocomplete('search', '');
+        }).keydown(function(e){
+            var key = e.which;
+            if (key == 13) {
+                EditFile($(this).val());
+            }
         });
     }
 };
