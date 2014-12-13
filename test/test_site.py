6f01fff6c6c07cb83cbacfc52b6ea741ff4e286a
zhouzhiming <zzmfish@163.com>
2014-12-13 17:18:23 +0800

    测试代码重构：封装TestSiteBase、TestSite和两个测试函数

--- test/test_site.py
+++ test/test_site.py
@@ -10,55 +10,62 @@ import selenium.webdriver as webdriver
 
 TEST_PORT = 19168
 
-class GoogleTestCase(unittest.TestCase):
+class TestSiteBase(unittest.TestCase):
 
-    def get_url(self, path):
+    server_pid = 0
+    browser = None
+
+    def get_url(self, path=''):
         return 'http://127.0.0.1:%d/%s' % (TEST_PORT, path)
 
-    def init_server(self):
+    @classmethod
+    def init_server(cls):
         pid = os.fork()
         if pid == 0:
             test_dir = os.path.dirname(__file__)
             src_dir = os.path.join(test_dir, '..')
             os.chdir(src_dir)
             python_path = os.popen('which python').read().strip('\r\n\r ')
-            python_srcs = []
+            python_srcs = list()
             python_srcs.append('import config')
             python_srcs.append('config.port=%d' % TEST_PORT)
             python_srcs.append('config.md_dir="%s"' % os.path.join(test_dir, 'md_dir'))
             python_srcs.append('import main')
             python_srcs.append('main.main()')
             os.execv(python_path, [python_path, '-c', ';'.join(python_srcs)])
-        self.addCleanup(self.stop_setver, pid)
+        cls.server_pid = pid
         time.sleep(1)
-
-    def stop_setver(self, server_pid):
-        os.kill(server_pid, signal.SIGINT)
-
-    def init_browser(self):
-        self.browser = webdriver.Firefox()
-        self.addCleanup(self.browser.quit)
 
     def delay(self):
         time.sleep(1)
 
-    def setUp(self):
-        self.init_server()
-        self.init_browser()
+    @classmethod
+    def setUpClass(cls):
+        cls.init_server()
+        cls.browser = webdriver.Firefox()
 
-    def test_case1(self):
-        self.browser.get(self.get_url(''))
-        self.delay()
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_pid:
+            os.kill(cls.server_pid, signal.SIGINT)
+        if cls.browser:
+            cls.browser.quit()
 
+
+class TestSite(TestSiteBase):
+
+    def test_editor(self):
+        self.browser.get(self.get_url())
         self.browser.find_element_by_id('Editor')
 
+    def test_file_input(self):
         # 点击文件名输入框
+        self.browser.get(self.get_url())
         file_input = self.browser.find_element_by_id('FileNameInput')
         file_input.click()
         self.delay()
         self.browser.find_element_by_class_name('ui-autocomplete')
         self.browser.find_element_by_xpath('//li[@class="ui-menu-item" and text() = "test1"]')
-
 
 
 if __name__ == '__main__':
