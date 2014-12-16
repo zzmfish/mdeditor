a2ad1fd6def52fd3b10b76e0eb207f1852ac0364
zhouzhiming <zzmfish@163.com>
2014-12-16 22:01:40 +0800

    递归子目录，显示所有md文件

--- file_manager.py
+++ file_manager.py
@@ -9,11 +9,19 @@ class FileManager:
         self.file_dir = config.md_dir
 
     def get_file_names(self):
+        # 递归子目录，列出md文件
         out_names = []
-        file_names = os.listdir(self.file_dir)
-        for file_name in file_names:
-            if file_name.endswith('.md'):
-                out_names.append(file_name[ : file_name.rfind('.')])
+        for root, dirs, files in os.walk(self.file_dir, followlinks=True):
+            # 不显示起始目录
+            if root.startswith(self.file_dir):
+                root = root[len(self.file_dir):]
+            root = root.strip(os.sep)
+            for file_name in files:
+                # 只显示md文件，但不显示扩展名
+                if file_name.endswith('.md'):
+                    display_name = file_name[ : file_name.rfind('.')]
+                    out_names.append(os.path.join(root, display_name))
+
         return out_names
 
 file_manager = FileManager()
