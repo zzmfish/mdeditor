04bbaa7a0877b55346d05c3c85f6e9e54877cd84
zhouzhiming <zzmfish@163.com>
2014-12-23 20:04:24 +0800

    缓存uml转换的图片数据

--- tools/code2img.py
+++ tools/code2img.py
@@ -8,7 +8,29 @@ import markdown
 #自定义
 import config
 
+class LRUCache:
+    def __init__(self, size=10):
+        self.cache = []
+        self.size = size
+
+    def load_data(self, key):
+        for i in range(len(self.cache)):
+            item = self.cache[i]
+            if item[0] == key:
+                # 如果命中则添加到列表最后，然后返回
+                del self.cache[i]
+                self.cache.append(item)
+                return item[1]
+        return None
+
+    def save_data(self, key, value):
+        while len(self.cache) >= self.size:
+            del self.cache[0]
+        self.cache.append((key, value))
+
 class Code2ImgPreprocessor(markdown.preprocessors.Preprocessor):
+    data_cache = LRUCache()
+
     def uml(self, code):
         pipe = subprocess.Popen(['java', '-jar', config.plantuml_path, '-pipe', '-tpng'], \
                 stdin = subprocess.PIPE, stdout = subprocess.PIPE)
@@ -25,11 +47,15 @@ class Code2ImgPreprocessor(markdown.preprocessors.Preprocessor):
             match = regx.search(text)
             if not match:
                 break
-            img_type = match.group(1)
-            img_code = match.group(2)
-            img_data = ''
-            if img_type == 'uml':
-                img_data = self.uml(img_code)
+            img_data = self.data_cache.load_data(match.group(0))
+            if not img_data:
+                img_type = match.group(1)
+                img_code = match.group(2)
+                img_data = ''
+                if img_type == 'uml':
+                    img_data = self.uml(img_code)
+                if img_data:
+                    self.data_cache.save_data(match.group(0), img_data)
             text = text.replace(match.group(0), '<img src="data:image/png;base64,%s" />' % base64.b64encode(img_data))
 
         return text.split('\n')
