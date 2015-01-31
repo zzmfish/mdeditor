37b9f8743d393fe2a666486c24e29e904640b8c1
zhouzhiming <zzmfish@163.com>
2015-01-31 11:07:20 +0800

    隐藏源码或渲染；显示源码主题选框

--- res/js/toolbar.js
+++ res/js/toolbar.js
@@ -4,6 +4,8 @@ var Toolbar = {
     init : function(files) {
         //获取控件
         this.fileNameInput = $("#FileNameInput");
+        this.showSourceInput = $('#ShowSource');
+        this.showRendererInput = $('#ShowRenderer');
 
         //获取文件列表
         this.files = files;
@@ -34,6 +36,20 @@ var Toolbar = {
             $(this).val(Editor.GetFileName());
         });
 
+        //显示源码
+        this.showSourceInput.change(function() {
+            if (!Toolbar.showRendererInput.prop('checked'))
+                $(this).prop('checked', true);
+            Toolbar._updateSourceAndRendererLayout();
+        });
+
+        //显示渲染
+        this.showRendererInput.change(function() {
+            if (!Toolbar.showSourceInput.prop('checked'))
+                $(this).prop('checked', true);
+            Toolbar._updateSourceAndRendererLayout();
+        });
+
         //根据url hash打开文件
         if (window.location.hash) {
             var fileName = unescape(window.location.hash.substr(1));
@@ -47,4 +63,21 @@ var Toolbar = {
         this.files.push(name);
         this.init(this.files);
     },
+    _updateSourceAndRendererLayout : function()
+    {
+        var showSource = this.showSourceInput.prop('checked');
+        var showRenderer = this.showRendererInput.prop('checked');
+        if (showSource && showRenderer) {
+            $('#EditorTd').css('width', '50%').css('display', '');
+            $('#RendererTd').css('width', '50%').css('display', '');
+        }
+        else if (showSource) {
+            $('#EditorTd').css('width', '100%').css('display', '');
+            $('#RendererTd').css('width', '0%').css('display', 'none');
+        }
+        else if (showRenderer) {
+            $('#EditorTd').css('width', '0%').css('display', 'none');
+            $('#RendererTd').css('width', '100%').css('display', '');
+        }
+    }
 };
