1d024262fcfdf1e73edb24c3c8b153d584a45a67
zhouzhiming <zzmfish@163.com>
2015-03-30 21:44:31 +0800

    支持静态图片目录

--- main.py
+++ main.py
@@ -13,16 +13,24 @@ import handler
 
 
 def main():
+    html_dir = config.html_dir
+    if type(html_dir) is str:
+        html_dir = html_dir.decode(config.fs_charset)
+
     tornado.log.enable_pretty_logging()
-    application = tornado.web.Application([
-        (r"/", handler.MainHandler),
-        (r"/GetFile", handler.GetFileHandler),
-        (r"/SaveFile", handler.SaveFileHandler),
-        (r"/Render", handler.RenderHandler),
-        (r"/css/.*", handler.ResourceHandler),
-        (r"/js/.*", handler.ResourceHandler),
-        (r"/lib/.*", handler.ResourceHandler),
-    ])
+    application = tornado.web.Application(
+        [
+            (r"/", handler.MainHandler),
+            (r"/GetFile", handler.GetFileHandler),
+            (r"/SaveFile", handler.SaveFileHandler),
+            (r"/Render", handler.RenderHandler),
+            (r"/css/.*", handler.ResourceHandler),
+            (r"/js/.*", handler.ResourceHandler),
+            (r"/lib/.*", handler.ResourceHandler),
+            (r"/(images/.*)", tornado.web.StaticFileHandler, {"path": html_dir}),
+        ],
+        static_path='.'
+    )
     application.listen(config.port)
     logging.info('start at port %d' % config.port)
     tornado.ioloop.IOLoop.instance().start()
