9b02f75dab4af057fbf0acb3cee89cc3c41b72a6
zhouzhiming <zzmfish@163.com>
2015-09-22 08:20:05 +0800

    网站编译使用树形控件

--- tools/make_site.py
+++ tools/make_site.py
@@ -33,7 +33,7 @@ for root, dirs, files in os.walk(config.md_dir, followlinks=False):
         if should_make_html:
             print '%s' % md_file
             print '  -> %s' % html_file
-            html = make_html.make_html(open(md_file, 'r').read(), 'style.css')
+            html = make_html.make_html(open(md_file, 'r').read(), '/style.css')
             open(html_file, 'w').write(html)
 
         rel_path = html_file
@@ -41,19 +41,61 @@ for root, dirs, files in os.walk(config.md_dir, followlinks=False):
             rel_path = rel_path[len(config.html_dir):]
         links.append(rel_path.strip(os.path.sep))
 
+files = [link[:-5] for link in links]
+print '\n'.join(files)
 index_path = os.path.join(config.html_dir, 'index.html')
 print 'writing %s' % index_path
 index_file = open(index_path, 'w')
 index_file.write('<html><head>')
 index_file.write('<title>%s</title>' % config.title)
 index_file.write('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />')
+index_file.write('''
+<link rel="stylesheet" href="res/jstree/themes/default/style.min.css" />
+<script src="res/jquery.js" type="text/javascript" charset="utf-8"></script>
+<script src="res/file_browser.js" type="text/javascript" charset="utf-8"></script>
+<script src="res/jstree/jstree.js" type="text/javascript" charset="utf-8"></script>
+''')
+
 index_file.write('</head>')
-index_file.write('<body>')
-index_file.write('<h1>%s</h1>' % config.title)
-index_file.write('<ul>')
-for link in links:
-    index_file.write('<li><a href="%s">%s</a></li>' % (link, link[:-5]))
-index_file.write('</ul>')
+index_file.write('<body style="padding:0px; margin: 0px;">')
+
+index_file.write('''
+<table style="width:100%; height:100%">
+    <tr>
+        <td style="vertical-align:top; width:20%; background-color: rgb(232, 233, 232);">
+            <div>搜索：<input type=text id="SearchInput" /></div>
+            <div id="FileBrowser" style="width:100%;"></div>
+        </td>
+        <td>
+            <iframe id="PageFrame" style="width:100%;height:100%;" frameborder="0" ></iframe>
+        </td>
+    </tr>
+</table>
+''');
+index_file.write("""
+<script>
+FileBrowser.update(["%s"]);
+FileBrowser.onOpen = function(filePath) {
+    document.getElementById('PageFrame').src = filePath + '.html';
+}
+var searchTimeout = undefined;
+$('#SearchInput').keyup(function () {
+    if(searchTimeout)
+        clearTimeout(searchTimeout);
+	searchTimeout = setTimeout(function () {
+        var v = $('#SearchInput').val();
+        FileBrowser.search(v);
+    }, 250);
+});
+
+</script>
+""" % '","'.join(files))
 index_file.write('</body></html>')
 index_file.close()
 
+res_dir = os.path.join(config.html_dir, 'res')
+if not os.path.exists(res_dir):
+    os.mkdir(res_dir)
+os.system('cp -rv res/lib/jquery.js %s/' % res_dir)
+os.system('cp -rv res/lib/jstree %s/' % res_dir)
+os.system('cp -rv res/js/file_browser.js %s/' % res_dir)
